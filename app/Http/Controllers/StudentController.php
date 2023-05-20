e53df56ea2f3a378104cf83ac7dc9abcf1d671c3
Ron Matthew Alunan <ron.alunan@usjr.edu.ph>
2023-05-20 14:25:45 +0800

    added student controller

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Return list of students
+     */
+    public function getStudents(Request $request)
+    {
+        // Retrieve all movies from the database
+        $students = Student::all();
+
+        // Return a JSON response containing the movies
+        return response()->json([
+            'data' => $students,
+        ]);
+    }
+    /**
+     * Get LMS usage based on student number
+     */
+    public function getStudentLMSUsage(Request $request, $stud_no)
+    {
+        $data = Student::with(['usage'])->find($stud_no);
+
+        if (!$data) {
+            return response()->json(['error' => 'Student not found'], 404);
+        }
+
+        return response()->json([
+            'data' => $data
+        ]);
+    }
+}
